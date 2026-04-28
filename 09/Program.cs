42c313129095d3f94a4680e2bc8c2faa1b7c06b4
Andy <Andy@ANDY>
2026-04-28 17:15:42 -0600

    ejercicio 9

---
+++ 09/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace _09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Determinar el valor menor contenido en una colección de números.
+            int [] numeros = { 4, 5, 6, 7, 8, 9 };
+            int menor = numeros[0];
+                        foreach (int numero in numeros) 
+            {
+                if (numero < menor)
+                {
+                    menor = numero;
+                }
+            }
+            Console.WriteLine(menor);
+        }
+    }
+}
