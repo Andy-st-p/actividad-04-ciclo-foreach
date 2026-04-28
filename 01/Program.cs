18134eaae52663f33b05ab61c22a0c1b42f7c2a9
Andy <Andy@ANDY>
2026-04-28 16:11:15 -0600

    Agregar archivos de proyecto.

---
+++ 01/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mostrar todos los valores contenidos en una colección de números ya definida.
+            int[] numeros = { 1, 2, 3, 4, 5 };
+            
+            foreach (int numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+
+        }
+    }
+}
---
+++ actividad 04 ciclo foreach/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace actividad_04_ciclo_foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
