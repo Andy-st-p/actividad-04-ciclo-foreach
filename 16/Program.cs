637cc61890806c2311e51c1317712b56807a0377
Andy <Andy@ANDY>
2026-04-29 22:58:18 -0600

    16

---
+++ 16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace _16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Contar cuántos valores cumplen una condición dada por el usuario.
+            int[] numeros = { 1, 6, 11, 15, 5, 16, 25 };
+            int contador = 0;
+            
+            foreach (int numero in numeros)
+            {
+                if (numero > 10)
+                {
+                    contador++;
+                }
+            }
+
+            Console.WriteLine($"Cantidad de valores que cumplen la condición son: {contador}");
+        }
+    }
+}
