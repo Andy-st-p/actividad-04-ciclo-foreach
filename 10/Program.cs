5d2492ecc13f92d2d43780131c3db19284379b06
Andy <Andy@ANDY>
2026-04-29 20:57:21 -0600

    nuevos ejercicios

---
+++ 10/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verificar si un número específico se encuentra dentro de una colección.
+            int[] numeros = { 4, 5, 6, 7, 8, 9 };
+            int numeroBuscado;
+            bool encontrado = false;
+
+            Console.WriteLine("Ingrese el número que desea buscar:");
+            numeroBuscado = int.Parse(Console.ReadLine());
+             
+            foreach (int numero in numeros)
+            {
+                if (numero == numeroBuscado)
+                {
+                    encontrado = true;
+                    break;
+                }
+            }
+
+            if (encontrado)
+            {
+                Console.WriteLine("El número {0} se encuentra en la colección.", numeroBuscado);
+            }
+            else
+            {
+                Console.WriteLine("El número {0} no se encuentra en la colección.", numeroBuscado);
+            }
+        }
+    }
+}
---
+++ 11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace _11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Contar cuántos valores están dentro de un rango definido.
+            int[] numeros = { 4, 5, 6, 7, 8, 9, 10 };
+            int contador = 0;
+
+            foreach (int numero in numeros)
+
+            {
+
+                Console.WriteLine("Ingrese el valor mínimo del rango:");
+                int valorMinimo = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el valor máximo del rango:");
+                int valorMaximo = int.Parse(Console.ReadLine());
+                if (numero >= valorMinimo && numero <= valorMaximo)
+                {
+                    contador++;
+
+                }
+
+                Console.WriteLine($"La cantidad de valores dentro del rango es: {contador}");
+                
+
+            }
+        }
+    }
+}
